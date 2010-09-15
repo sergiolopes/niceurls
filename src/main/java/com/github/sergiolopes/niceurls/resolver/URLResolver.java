68df8b25958c2e9c975a843a82fe32fc86edd986
Sergio Lopes <slopes@gmail.com>
2010-09-15 04:31:44 -0300

    naive cache in URLResolver

--- src/main/java/com/github/sergiolopes/niceurls/resolver/URLResolver.java
+++ src/main/java/com/github/sergiolopes/niceurls/resolver/URLResolver.java
@@ -2,6 +2,8 @@ package com.github.sergiolopes.niceurls.resolver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 
 import org.apache.log4j.Logger;
@@ -17,6 +19,9 @@ import com.github.sergiolopes.niceurls.results.ResultStrategy;
 public class URLResolver {
 
 	private final static Logger logger = Logger.getLogger(URLResolver.class);
+	
+	// TODO change this to some LFU impl
+	private final Map<String, Result> urlToResultCache = new ConcurrentHashMap<String, Result>();
 	private final List<Route> routes;
 	private Result nullResult = new Result(RouteType.IGNORE.getResultStrategy());
 	
@@ -41,22 +46,34 @@ public class URLResolver {
 	public Result resolveURL(String url) {
 		if (logger.isTraceEnabled()) logger.trace("Trying to resolve " + url);
 		
-		for (Route route : this.routes) {
-			Matcher m = route.getFromPattern().matcher(url);
-			if (m.matches()) {
-				
-				ParamsContext paramsContext = new ParamsContext();
-				// parse variables
-				int i = 1;
-				for (String paramName : route.getParamNames()) {
-					paramsContext.addParameter(paramName, m.group(i++));
-				}
+		Result result = urlToResultCache.get(url);
+		if (result == null) {
+			for (Route route : this.routes) {
+				Matcher matcher = route.getFromPattern().matcher(url);
+				if (matcher.matches()) {
+					ParamsContext paramsContext = new ParamsContext();
+					
+					// parse variables
+					int i = 1;
+					for (String paramName : route.getParamNames()) {
+						paramsContext.addParameter(paramName, matcher.group(i++));
+					}
 
-				String uri = route.evaluateTo(paramsContext);
-				ResultStrategy resultStrategy = route.getType().getResultStrategy();
-				return new Result(paramsContext, resultStrategy, uri);
+					String uri = route.evaluateTo(paramsContext);
+					ResultStrategy resultStrategy = route.getType().getResultStrategy();
+					result = new Result(paramsContext, resultStrategy, uri);
+					
+					urlToResultCache.put(url, result);
+					break;
+				}
 			}
+			
+		} else {
+			if (logger.isTraceEnabled()) 
+				logger.trace("Found in cache ");
 		}
+
+		if (result != null) return result;
 		
 		if (logger.isTraceEnabled()) logger.trace("NiceURL doesn't know this URL");
 		return nullResult;
